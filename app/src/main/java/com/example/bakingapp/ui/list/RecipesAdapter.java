c87626c71e0a609ef52aed27637804eb05016ddd
tariqywsf <tarek.buhdeima@gmail.com>
2021-01-03 20:17:10 +0200

    add launcher icon make base for adapter

---
+++ app/src/main/java/com/example/bakingapp/ui/list/RecipesAdapter.java
@@ -0,0 +1,8 @@
+package com.example.bakingapp.ui.list;
+
+//
+//public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder> {
+//
+//    public class RecipesViewHolder extends RecyclerView.ViewHolder {
+//    }
+//}
